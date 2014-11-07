1966790262312b210f3941c3d9ed36c049d7722f
Johannes Bayerl <jm.bayerl@googlemail.com>
2014-11-07 13:35:07 +0100

    added location converter

---
+++ src/org/wahlzeit/location/Implementation/LocationConverter.java
@@ -0,0 +1,26 @@
+package org.wahlzeit.location.Implementation;
+
+import com.mapcode.Mapcode;
+
+public class LocationConverter {
+	private Mapcode mapCode = null;
+	private GPSLocation gpsLocation = null;
+	
+	
+	public String getMapCodeFromGPS(String GPS){
+		
+//		mapcodeCoordinate = new Mapcode("4J.RB", Territory.FRA);
+//		String iso = mapcodeCoordinate.asInternationalISO();
+//		final String mapcode2 = "NLD 49.4V";
+//		final Point p = MapcodeCodec.decode(mapcode2);
+//
+//		double lat = p.getLatDeg();
+//		double lon = p.getLonDeg();
+		
+		return "";
+	}
+	
+	public String getGPSFromMapCode(String MapCodeString){
+		return "";
+	}
+}
